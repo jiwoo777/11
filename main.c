612bc44d6a3f521e76f342671787da6e853fb459
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-11-12 15:58:27 +0900

    practice 4-2

--- main.c
+++ main.c
@@ -3,17 +3,19 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-char *proverb = "Allthat glisters is not gold.";
-void setPointer(char *q)
+char *proverb = "All that glisters is not gold.";
+
+void setPointer(char **q)
 {
-	q = proverb;
+	*q = proverb;
 }
 
 int main(int argc, char *argv[])
  {
  	char *p = "zzz";
- 	setPointer(p);
+ 	setPointer(&p);
  	printf("%s\n", p);
  	
 	return 0;
 }
+

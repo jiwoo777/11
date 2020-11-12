1f8043e4e20889f4b35ef0064d5f705b47d303d7
jiwoo01 <gwangbok1004@sookmyung.ac.kr>
2020-11-12 16:31:19 +0900

    practice 3-2

--- main.c
+++ main.c
@@ -2,53 +2,20 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-void print_image(int a[5][5])
+int main (void)
 {
-	int i,j;
+	int i =100;
+	int *p = &i;
+	int **q = &p;
 	
-	for(i=0; i<5; i++)
-    {	
-		for(j=0; j<5; j++)
-		{
-		printf("%d", a[i][j]);
-	    }
-	    printf("\n");
-	}
-	printf("\n");
-}
-
-void brighten_image(int a[5][5])
-{
-	int i, j;
-	int *ptr;
+	*p = 200;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 	
-	ptr = (int*)a[0];
+	**q = 300;
+	printf("i = %d, *p = %d, **q = %d\n", i, *p, **q);
 	
-	for(i=0; i<5; i++)
-	{
-		for(j=0; j<5; j++)
-		{
-			*ptr += 10;
-			ptr++;
-		}
-		
-	}
-}
-
-int main(int argc, char *argv[])
- {
- 	int image[5][5] = {
-	 {10,20,30,40,50},
-	 {10,20,30,40,50},
-	 {10,20,30,40,50},
-	 {10,20,30,40,50},
-	 {10,20,30,40,50}
-	 };
-	 
-	 print_image(image);
-	 brighten_image(image);
-	 print_image(image);
-	 
-	 return 0;
+	printf("%p %p\n", p, *q);
+	
+	return 0;
 }
 
